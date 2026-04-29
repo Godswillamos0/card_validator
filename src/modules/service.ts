e3fe1e911985d98d12e821efa88cf6026831ef59
Godswill Amos <amosgodswill00@gmail.com>
2026-04-29 10:40:58 +0100

    credit-card-type

--- src/modules/service.ts
+++ src/modules/service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { validateLuhn } from '../common/utils/luhn.util';
+import creditCardType = require('credit-card-type');
 
 @Injectable()
 export class CardValidationService {
     validateCard(cardNumber: string) {
         const isValid = validateLuhn(cardNumber);
+        
+        // Get card type using the credit-card-type library
+        const cardTypes = creditCardType(cardNumber);
+        const cardType = cardTypes.length > 0 ? cardTypes[0].niceType : null;
 
         if (!isValid) {
-            return { isValid: false, cardType: null, message: 'Luhn checksum failed' };
+            return { 
+                isValid: false, 
+                cardType: cardType, 
+                message: 'Invalid check digit (Luhn algorithm failure)' 
+            };
         }
 
-        const cardType = this.getCardType(cardNumber);
         return {
             isValid: true,
             cardType,
             message: cardType ? 'Valid card number' : 'Valid number but unknown issuer',
         };
-    }
-
-    private getCardType(number: string): string | null {
-        if (/^4/.test(number)) return 'Visa';
-        if (/^5[1-5]/.test(number) || /^2[2-7]/.test(number)) return 'Mastercard';
-        if (/^(506[01]|507[89]|6500)/.test(number)) return 'Verve';
-        return null;
     }
 }
