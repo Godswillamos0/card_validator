45c5fa91c19ec3893d381f7877cc888b0c2b594c
Godswill Amos <amosgodswill00@gmail.com>
2026-04-29 11:20:35 +0100

    reformated cardNumber

--- src/modules/dto/dto.ts
+++ src/modules/dto/dto.ts
@@ -12,5 +12,5 @@ export class ValidateCardDto {
     @MinLength(13)
     @MaxLength(19)
     @Matches(/^[0-9]+$/, { message: 'Card number must contain only digits' })
-    cardNumber: string;
+    cardNumber!: string;
 }
